2e9dbf25e7a4a214cdee84eb436b4352db053e9f
Nasr-Eddine Farid <nafarid@c1r5p1.1337.ma>
2026-02-12 13:50:28 +0100

    x

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -2,10 +2,30 @@ import { Mail, Github, Linkedin, MapPin } from "lucide-react";
 import SectionWrapper from "./SectionWrapper";
 
 const links = [
-  { icon: Mail, label: "Email", value: "nasrdinefarid@gmail.com", href: "https://mail.google.com" },
-  { icon: Github, label: "GitHub", value: "github.com/faridnasrdine", href: "https://github.com" },
-  { icon: Linkedin, label: "LinkedIn", value: "linkedin.com/in/nasreddine", href: "https://linkedin.com" },
-  { icon: MapPin, label: "Location", value: "Morocco 🇲🇦", href: "#" },
+  { 
+    icon: Mail, 
+    label: "Email", 
+    value: "nasrdinefarid@gmail.com", 
+    href: "mailto:nasrdinefarid@gmail.com"
+  },
+  { 
+    icon: Github, 
+    label: "GitHub", 
+    value: "github.com/faridnasrdine", 
+    href: "https://github.com/faridnasrdine"
+  },
+  { 
+    icon: Linkedin, 
+    label: "LinkedIn", 
+    value: "linkedin.com/in/nasr-eddine-farid/", 
+    href: "https://linkedin.com/in/nasr-eddine-farid"
+  },
+  { 
+    icon: MapPin, 
+    label: "Location", 
+    value: "Morocco 🇲🇦", 
+    href: "#"
+  },
 ];
 
 const ContactSection = () => (
